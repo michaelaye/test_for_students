52f555110d57a976042af045adb44b0fc0a06faf
kyro-D <kyle.rose@colorado.edu>
2016-07-01 14:03:56 -0600

    Update bad_python.py
    Update the function to convert inputs to floats before printing them.

--- bad_python.py
+++ bad_python.py
@@ -1,9 +1,13 @@
 def myfunc(a, b):
+    a = float(a) #convert input to float
+    b = float(b) #convert input to float
     print(a, b)
 
 
 # using myfunc:
 
 a = 1
+b = 2
 
-myfunc(a)
+
+myfunc(a, b)
