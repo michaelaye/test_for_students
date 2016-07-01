e653462d54cc71a23deb2b09530eb3882cb46e92
mafu4674 <mafu4674@colorado.edu>
2016-07-01 14:13:22 -0600

    Update bad_python.py

--- bad_python.py
+++ bad_python.py
@@ -1,7 +1,7 @@
-def myfunc(a, b):
-    print(a, b)
-
-
+def myfunc(a, *b):
+        print(a, *b)
+# A star is added to the b parameter to make it optional.        
+        
 # using myfunc:
 
 a = 1
